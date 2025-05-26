ca9c0b2f15ac3450a3f75ab15f8ff98a108543fc
Younus Mowlana <younusm@infyinnos.com>
2025-05-26 11:50:31 +0530

    changes

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -9,7 +9,7 @@ const menuItems = [
       {
         icon: "/home.png",
         label: "Home",
-        href: "/",
+        href: "/admin",
         visible: ["admin", "teacher", "student", "parent"],
       },
       {
